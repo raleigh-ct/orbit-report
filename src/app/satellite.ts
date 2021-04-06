878b3e2501e914c346baba34d6a39c2ba8da3bcb
Raleigh Moore <raleighcmoore@gmail.com>
2021-04-06 09:01:32 -0500

    finished project

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,13 +1,23 @@
 export class Satellite {
-
     name: string;
     orbitType: string;
     type: string;
     operational: boolean;
     launchDate: string;
 
-    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
-        
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) { 
+        this.name = name;
+        this.type = type;
+        this.launchDate = launchDate;
+        this.operational = operational;
+        this.orbitType = orbitType;
     }
+    shouldShowWarning () {
+        if (this.type.toLowerCase() === 'space debris') {
+            return true;
+        } else {
+            return false;
+        }
+        }
 
-}
+    }
